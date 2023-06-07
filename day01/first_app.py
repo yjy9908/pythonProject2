34d2e970fd613f03a46206fae7f37fc32cf3b68e
yjy <yjy@com>
2023-06-07 14:46:30 +0800

    '第一次提交'

---
+++ day01/first_app.py
@@ -0,0 +1,13 @@
+# 字典
+from appium import webdriver
+
+desired_caps={
+    "platformName":"Android",
+    "platformVersion":"6.0.1",
+    "deviceName":"yjy",
+    "appPackage":"com.km.karaoke",
+    "appActivity":"com.utalk.hsing.activity.StartActivity",
+    "noReset":True
+}
+
+driver=webdriver.Remote("http://127.0.0.1:4723/wd/hub",desired_caps)
